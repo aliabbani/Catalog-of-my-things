7465ee2eb5daca73a2a450fac611e525cb54369d
rpire <rubenpire7@gmail.com>
2021-11-22 10:00:25 -0400

    Add required methods to the Item class

--- item.rb
+++ item.rb
@@ -1,8 +1,25 @@
+require 'date'
+
 class Item
-    def initialize(genre, author, label, publish_date, archived = false)
+    DEFAULT_BOOL = false.freeze
+
+    attr_accessor :archived, :publish_date
+
+    def initialize(genre, author, label, publish_date, archived = DEFAULT_BOOL)
         @id = Random.rand(1..1000)
         @author = author
         @label = label
-        @publish_date = publish_date
+        date_arr = publish_date.split('/')
+        date_arr = date_arr.map { |input| input.to_i }
+        @publish_date = Date.new(date_arr.first, date_arr[1], date_arr.last)
+        @archived = archived
+    end
+
+    def can_be_archived?
+        Date.today > @publish_date.next_year(10)
+    end
+
+    def move_to_archive
+        @archived = can_be_archived?
     end
 end
