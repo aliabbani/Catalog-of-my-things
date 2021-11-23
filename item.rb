6fff72c3fb171b7657921d44bc58588b7e4a4a73
Amine Smahi <md.amine.smahi@gmail.com>
2021-11-23 09:35:09 +0100

    Make can_be_archied private

--- item.rb
+++ item.rb
@@ -1,25 +1,26 @@
 require 'date'
 
 class Item
-  DEFAULT_BOOL = false
+  attr_accessor :publish_date
 
-  attr_accessor :archived, :publish_date
-
-  def initialize(_genre, author, label, publish_date, archived = DEFAULT_BOOL)
+  def initialize(genre, author, label, publish_date)
     @id = Random.rand(1..1000)
+    @genre = genre
     @author = author
     @label = label
     date_arr = publish_date.split('/')
     date_arr = date_arr.map(&:to_i)
     @publish_date = Date.new(date_arr.first, date_arr[1], date_arr.last)
-    @archived = archived
-  end
-
-  def can_be_archived?
-    Date.today > @publish_date.next_year(10)
+    @archived = false
   end
 
   def move_to_archive
     @archived = can_be_archived?
+  end
+
+  private
+
+  def can_be_archived?
+    Date.today > @publish_date.next_year(10)
   end
 end
