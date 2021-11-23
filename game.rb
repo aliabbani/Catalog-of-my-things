c741adfd4957772c0e6b778ff1c47cf775f3f3e5
Amine Smahi <md.amine.smahi@gmail.com>
2021-11-23 12:37:52 +0100

    Create 2 classes Author and Game with their methods

---
+++ game.rb
@@ -0,0 +1,16 @@
+require_relative 'item'
+require 'date'
+
+class Game < Item
+  def initialize(publish_date, multi_player, last_player_at)
+    super(publish_date)
+    @multi_player = multi_player
+    @last_player_at = last_player_at
+  end
+
+  private
+
+  def can_be_archived?
+    super() && Date.today > @last_player_at.next_year(2)
+  end
+end
