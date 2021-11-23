bb4953953d6d5f8b775cef3cb9e13e7c8d2add85
ali abbani <aliox@ahlam.com>
2021-11-23 15:07:53 +0200

    Create a label class and implement an add-item method inside it

---
+++ label.rb
@@ -0,0 +1,15 @@
+class Label
+    attr_reader :title, :color
+
+    def initialize(title, color)
+        @id = Random.rand(1..1000)
+        @title = title
+        @color = color
+        @items = []
+    end
+
+    def add_item(item)
+        item.label = self
+        @items << item
+    end
+end
