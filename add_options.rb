d70d7687ac6544d35d2620bc470980ed4ee5040b
ali abbani <aliox@ahlam.com>
2021-11-24 11:56:49 +0200

    Add add_option file

---
+++ add_options.rb
@@ -0,0 +1,44 @@
+module AddOptions
+  INPT_MSG = 'Enter your option number here --> '.freeze
+  ENTR_MSG = "\nPress ENTER to continue".freeze
+
+  def initialize
+    @add_item_option = 0
+  end
+
+  def add_item_options
+    puts "\nWhich items do you wish to add?"
+    puts '1 - Add Book'
+    puts '2 - Add Music album'
+    puts '3 - Add Game'
+  end
+
+  def add_item_select
+    case @add_item_option
+    when 1
+      add_book
+    when 2
+      add_music_album
+    when 3
+      add_game
+    else
+      "\nSorry, I didn\'t get you quite well..."
+    end
+  end
+
+  def add_item
+    until [1, 2, 3].include?(@add_item_option)
+      add_item_options
+      print INPT_MSG
+      @add_item_option = gets.chomp.to_i
+      add_item_select
+    end
+    @add_item_option = 0
+  end
+
+  def add_music_album
+    puts "\nAdd of music albums:"
+    print ENTR_MSG
+    gets
+  end
+end
