a8bf2aa5d0f494640fd00d1cd5ef44a6835c5922
ali abbani <aliox@ahlam.com>
2021-11-24 20:20:21 +0200

    Fix Linters

--- main.rb
+++ main.rb
@@ -55,7 +55,7 @@ class App
   end
 
   def main
-    get_books
+    parse_books
     until @option == 6
       display_options
       print INPT_MSG
@@ -70,7 +70,7 @@ class App
     end
   end
 
-  def get_books
+  def parse_books
     File.open('books.json', 'w') { |f| f.write JSON.generate([]) } unless File.exist? 'books.json'
 
     JSON.parse(File.read('books.json')).map do |book|
@@ -81,7 +81,8 @@ class App
   def save_books
     @json_books = []
     @books.each do |book|
-      @json_books.push({ 'publisher' => book.publisher, 'cover_state' => book.cover_state, 'publish_date' =>book.publish_date })
+      @json_books.push({ 'publisher' => book.publisher, 'cover_state' => book.cover_state,
+                         'publish_date' => book.publish_date })
     end
     File.write('books.json', JSON.generate(@json_books))
   end
