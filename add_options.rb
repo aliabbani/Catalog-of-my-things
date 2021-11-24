66d40b8d0b319c8585ef047b4cbf9ce2b48f543f
Amine Smahi <md.amine.smahi@gmail.com>
2021-11-24 11:54:33 +0100

    Crate add_game method

--- add_options.rb
+++ add_options.rb
@@ -1,3 +1,6 @@
+require 'date'
+require './game.rb'
+
 module AddOptions
   INPT_MSG = 'Enter your option number here --> '.freeze
   ENTR_MSG = "\nPress ENTER to continue".freeze
@@ -40,5 +43,28 @@ module AddOptions
     puts "\nAdd of music albums:"
     print ENTR_MSG
     gets
+  end
+
+  def add_game
+    puts "Pleae enter the publish date of the game:"
+    date = enter_date
+    publish_date = Date.new(date[0], date[1], date[2])
+    puts "Is it multi_player:"
+    multi_player = gets.chomp
+    puts "the date of the last player"
+    date = enter_date
+    last_player_at = Date.new(date[0], date[1], date[2])
+    @games << Game.new(publish_date, multi_player, last_player_at)
+    puts "Game is added succussfully"
+  end
+
+  def enter_date
+    puts "Year: "
+    year = gets.chomp
+    puts "Month: "
+    month = gets.chomp
+    puts "Day: "
+    day = gets.chomp
+    [year.to_i, month.to_i, day.to_i]
   end
 end
